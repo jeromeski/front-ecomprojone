0e757ac234b94e3955fd8800ba7262851103d0b3
Jeromeski <jeromegee@gmail.com>
2020-06-30 12:52:26 +0800

    Environment variables

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Layout from './Layout';
 
+
 const Home = () => {
   return (
-    <Layout title='Home Page' description='Node React E-commerce App'/>
+    <Layout title='Home Page' description='Node React E-commerce App'></Layout>
   );
 };
 
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Layout from '../core/Layout';
+// import { API } from '../config';
 
 const Signup = () => {
   return (
-    <Layout title='Signup' description='Signup to Node React E-commerce App'/>
+    <Layout title='Signup' description='Signup to Node React E-commerce App'>{process.env.REACT_APP_API_URL}</Layout>
   );
 };
 
