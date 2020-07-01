75e70ff601fd6dbd3f20f633b436c1daf3b7f62a
Jeromeski <jeromegee@gmail.com>
2020-07-01 08:04:15 +0800

    User SignIn component and signin feature

--- src/auth/index.js
+++ src/auth/index.js
@@ -16,3 +16,20 @@ export const signup = user => {
       return console.log(err);
     });
 };
+
+export const signin = user => {
+  return fetch(`${API}/signin`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => {
+      return console.log(err);
+    });
+};
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,9 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
+import { signin } from '../auth';
 
 const Signin = () => {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    error: '',
+    loading: false,
+    redirectToReferrer: false
+  });
+
+  const { email, password, loading, error, redirectToReferrer } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = event => {
+    event.preventDefault();
+    setValues({...values, error: false, loading: true})
+    signin({ email, password }).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        setValues({
+          ...values,
+          loading: false,
+          redirectToReferrer: true
+        });
+      }
+    });
+  };
+
+  const signInForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input
+          type='email'
+          className='form-control'
+          onChange={handleChange('email')}
+          value={email}
+        />
+      </div>
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input
+          type='password'
+          className='form-control'
+          onChange={handleChange('password')}
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className='btn btn-primary'>
+        Submit
+      </button>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className='alert alert-danger'
+      style={{ display: error ? '' : 'none' }}>
+      {error}
+    </div>
+  );
+
+  const showLoading = () => (
+    loading && (
+      <div
+      className='alert alert-info'>
+      Loading!
+    </div>
+    )
+  );
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      return <Redirect to='/' />
+    }
+  }
+
   return (
-    <Layout title='Signin' description='Signin to Node React E-commerce App'/>
+    <Layout
+      title='Signup'
+      description='Signup to Node React E-commerce App'
+      className='container'
+      col-8
+      offset-2>
+      {showLoading()}
+      {showError()}
+      {signInForm()}
+      {redirectUser()}
+    </Layout>
   );
 };
 
