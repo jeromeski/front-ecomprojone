8ddeca8796a0f0ee8bee8d4a9e2524da57643ff2
Jeromeski <jeromegee@gmail.com>
2020-07-03 06:31:54 +0800

    Get categories in shop page

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from './Layout';
 import Card from './Card';
+import { getCategories } from './apiCore';
 
 const Shop = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false)
+
+  const init = () => {
+    getCategories().then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCategories(data)
+      }
+    });
+  };
+
+  useEffect(() => {
+    init()
+  },[])
+
   return (
     <Layout
       title='Shop Page'
       description='Search and find books of your choice'>
       <div className='row'>
-        <div className='col-4'>left sidebar</div>
+        <div className='col-4'>{JSON.stringify(categories)}</div>
         <div className='col-8'>right sidebar</div>
       </div>
     </Layout>
