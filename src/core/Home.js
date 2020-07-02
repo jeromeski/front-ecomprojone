5439ae579d17fb252795238df50c06c434b6f65b
Jeromeski <jeromegee@gmail.com>
2020-07-02 19:48:13 +0800

    Show products in card

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,42 +1,51 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 import { getProducts } from './apiCore';
-
+import Card from './Card';
 
 const Home = () => {
-  const [productsBySell, setProductsBySell] = useState([])
-  const [productsByArrival, setProductsByArrival] = useState([])
-  const [ error, setError] = useState(false)
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
 
   const loadProductsBySell = () => {
     getProducts('sold').then(data => {
-      if(data.error) {
-        setError(data.error)
+      if (data.error) {
+        setError(data.error);
       } else {
-        setProductsBySell(data)
+        setProductsBySell(data);
       }
-    })
-  }
+    });
+  };
 
   const loadProductsByArrival = () => {
     getProducts('createdAt').then(data => {
-      if(data.error) {
-        setError(data.error)
+      if (data.error) {
+        setError(data.error);
       } else {
-        setProductsByArrival(data)
+        setProductsByArrival(data);
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     loadProductsByArrival();
     loadProductsBySell();
-  }, [])
+  }, []);
   return (
     <Layout title='Home Page' description='Node React E-commerce App'>
-    {JSON.stringify(productsByArrival)}
-    <hr/>
-    {JSON.stringify(productsBySell)}
+      <h2 className='mb-4'>New Arrivals</h2>
+      <div className='row'>
+        {productsByArrival.map((products, i) => (
+          <Card key={i} product={products} />
+        ))}
+      </div>
+      <h2 className='mb-4'>Best Sellers</h2>
+      <div className='row'>
+        {productsBySell.map((products, i) => (
+          <Card key={i} product={products} />
+        ))}
+      </div>
     </Layout>
   );
 };
