cabb0c64d3bb5cfcc80b762ea05faac18a3797df
Jeromeski <jeromegee@gmail.com>
2020-07-04 09:14:39 +0800

    Show products by filter on shop page

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Layout from './Layout';
 import Card from './Card';
-import { getCategories } from './apiCore';
+import { getCategories, getFilteredProducts } from './apiCore';
 import Checkbox from './Checkbox';
 import { prices } from './fixedPrices';
 import RadioBox from './RadioBox';
@@ -9,6 +9,9 @@ import RadioBox from './RadioBox';
 const Shop = () => {
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState(false);
+  const [limit, setLimit] = useState(6);
+  const [skip, setSkip] = useState(0);
+  const [filteredResults, setFilteredResults] = useState(0);
   const [myFilters, setMyFilters] = useState({
     filters: {
       category: [],
@@ -26,6 +29,17 @@ const Shop = () => {
     });
   };
 
+  const loadFilteredResults = newFilters => {
+    // console.log(newFilters)
+    getFilteredProducts(skip, limit, newFilters).then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults(data);
+      }
+    });
+  };
+
   useEffect(() => {
     init();
   }, []);
@@ -34,11 +48,11 @@ const Shop = () => {
     const newFilters = { ...myFilters };
     newFilters.filters[filterBy] = filters;
 
-    if (filterBy == 'price') {
+    if (filterBy === 'price') {
       let priceValues = handlePrice(filters);
       newFilters.filters[filterBy] = priceValues;
     }
-
+    loadFilteredResults(myFilters.filters);
     setMyFilters(newFilters);
   };
 
@@ -53,6 +67,8 @@ const Shop = () => {
     }
     return array;
   };
+
+  
 
   return (
     <Layout
@@ -75,7 +91,7 @@ const Shop = () => {
             />
           </div>
         </div>
-        <div className='col-8'>{JSON.stringify(myFilters)}</div>
+        <div className='col-8'>{JSON.stringify(filteredResults)}</div>
       </div>
     </Layout>
   );
