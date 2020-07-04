d6849b4de8b08391a61d156dbc0e83341178291b
Jeromeski <jeromegee@gmail.com>
2020-07-05 07:30:08 +0800

    Product detail on single product page

--- src/core/Product.js
+++ src/core/Product.js
@@ -1,41 +1,46 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from 'react';
-import Layout from './Layout';
-import { read } from './apiCore';
-import Card from './Card';
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import { read } from "./apiCore";
+import Card from "./Card";
 
-const Product = (props) => {
-  const [product, setProduct] = useState({});
-  const [error, setError] = useState(false);
+const Product = props => {
+    const [product, setProduct] = useState({});
+    const [error, setError] = useState(false);
 
-  const loadSingleProduct = productId => {
-    read(productId).then(data => {
-      if (data.error) {
-        setError(data.error);
-      } else {
-        setProduct(data);
-      }
-    });
-  };
+    const loadSingleProduct = productId => {
+        read(productId).then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProduct(data);
+            }
+        });
+    };
 
-  useEffect(() => {
-    // grab the product id from react router dom - props url
-    const productId = props.match.params.productId;
-    loadSingleProduct(productId);
-  }, []);
+    useEffect(() => {
+        const productId = props.match.params.productId;
+        loadSingleProduct(productId);
+    }, [props]);
 
-  return (
-    <Layout
-      title={product && product.name}
-      description={
-        product && product.description && product.description.substring(0, 100)
-      }
-      className='container-fluid'>
-      <div className='row'>
-        {product && product.description && <Card product={product} showViewProductButton={false} />}
-      </div>
-    </Layout>
-  );
+    return (
+        <Layout
+            title={product && product.name}
+            description={
+                product &&
+                product.description &&
+                product.description.substring(0, 100)
+            }
+            className="container-fluid"
+        >
+            <div className="row">
+                <div className="col-8">
+                    {product && product.description && (
+                        <Card product={product} showViewProductButton={false} />
+                    )}
+                </div>
+            </div>
+        </Layout>
+    );
 };
 
 export default Product;
