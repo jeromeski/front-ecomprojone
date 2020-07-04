07970bf9f905fbe3515b5cf676da174f4ca148fb
Jeromeski <jeromegee@gmail.com>
2020-07-04 10:33:22 +0800

    Pass products to card component

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -11,7 +11,7 @@ const Shop = () => {
   const [error, setError] = useState(false);
   const [limit, setLimit] = useState(6);
   const [skip, setSkip] = useState(0);
-  const [filteredResults, setFilteredResults] = useState(0);
+  const [filteredResults, setFilteredResults] = useState([]);
   const [myFilters, setMyFilters] = useState({
     filters: {
       category: [],
@@ -35,13 +35,14 @@ const Shop = () => {
       if (data.error) {
         setError(data.error);
       } else {
-        setFilteredResults(data);
+        setFilteredResults(data.data);
       }
     });
   };
 
   useEffect(() => {
     init();
+    loadFilteredResults(skip, limit, myFilters.filters);
   }, []);
 
   const handleFilters = (filters, filterBy) => {
@@ -68,8 +69,6 @@ const Shop = () => {
     return array;
   };
 
-  
-
   return (
     <Layout
       title='Shop Page'
@@ -91,7 +90,14 @@ const Shop = () => {
             />
           </div>
         </div>
-        <div className='col-8'>{JSON.stringify(filteredResults)}</div>
+        <div className='col-8'>
+          <h2 className='mb-4'>Products</h2>
+          <div className='row'>
+            {filteredResults.map((product, i) => (
+              <Card product={product} key={i} />
+            ))}
+          </div>
+        </div>
       </div>
     </Layout>
   );
