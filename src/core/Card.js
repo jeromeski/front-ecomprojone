24fdfbfea1e39b3b16e5c1efea74280b0fe199f1
Jeromeski <jeromegee@gmail.com>
2020-07-05 11:32:38 +0800

    Add to cart

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImg';
 import moment from 'moment';
+import { addItem } from './cartHelper';
 
 const Card = ({ product, showViewProductButton = true }) => {
+  const [redirect, setRedirect] = useState(false)
   const showViewButton = showViewProductButton => {
     return (
       showViewProductButton && (
@@ -24,10 +26,31 @@ const Card = ({ product, showViewProductButton = true }) => {
     );
   };
 
+  const addToCart = () => {
+    addItem(product, () => {
+      setRedirect(true)
+    })
+  }
+
+  const shouldRedirect = redirect => {
+    if(redirect) {
+      return <Redirect to='/cart' />
+    }
+  }
+
+  const showAddToCartButton = () => {
+    return (
+      <button onClick={addToCart} className="btn btn-outline-warning">
+        Add to cart
+      </button>
+    )
+  }
+
   return (
     <div className='card'>
       <div className='card-header name'>{product.name}</div>
       <div className='card-body'>
+        {shouldRedirect(redirect)}
         <ShowImage item={product} url='product' />
         <p className='lead mt-2'>{product.description.substring(0, 100)}</p>
         <p className='black-10'>${product.price}</p>
@@ -42,7 +65,8 @@ const Card = ({ product, showViewProductButton = true }) => {
         <br />
 
         {showViewButton(showViewProductButton)}
-        <button className='btn btn-outline-warning mt-2 mb-2'>Add to cart</button>
+        {showAddToCartButton()}
+
       </div>
     </div>
   );
