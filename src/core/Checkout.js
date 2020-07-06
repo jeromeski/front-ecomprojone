890b960d891375a30d85358196235c45d32fe6b0
Jeromeski <jeromegee@gmail.com>
2020-07-06 16:09:20 +0800

    Braintree setup frontend

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -1,22 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
+import { getBraintreeClientToken } from './apiCore';
+import DropIn from 'braintree-web-drop-in-react';
 
 const Checkout = ({ products }) => {
-
-  const showCheckout = () => isAuthenticated() ? (
-    <button className='btn btn-success'>Checkout</button>
-  ) : (
-    <Link to='/signin'>
-      <button className='btn btn-primary'>Sign in to checkout</button>
-    </Link>
-  )
+  const [data, setData] = useState({
+    success: false,
+    clientToken: null,
+    error: '',
+    instance: {},
+    address: ''
+  });
 
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
     }, 0);
   };
+
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
+  const getToken = (userId, token) => {
+    getBraintreeClientToken(userId, token).then(data => {
+      if (data.error) {
+        setData({ ...data, error: data.error });
+      } else {
+        setData({ ...data, clientToken: data.clientToken });
+      }
+    });
+  };
+
+  useEffect(() => {
+    getToken(userId, token);
+  }, []);
+
+  const showCheckout = () =>
+    isAuthenticated() ? (
+      <div>{showDropIn()}</div>
+    ) : (
+      <Link to='/signin'>
+        <button className='btn btn-primary'>Sign in to checkout</button>
+      </Link>
+    );
+
+  const showDropIn = () => (
+    <div>
+      {data.clientToken !== null && products.length > 0 ? (
+        <div>
+          <DropIn
+            options={{
+              authorization: data.clientToken
+            }}
+            onInstance={instance => (data.instance = instance)}
+          />
+          <button className='btn btn-success'>Checkout</button>
+        </div>
+      ) : null}
+    </div>
+  );
+
   return (
     <div>
       <h2>Total: ${getTotal()}</h2>
